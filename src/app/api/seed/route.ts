47db2aa833c7dd5897b8d32143bb5f4874a0a0b1
✿ corey (they/them) <c@rey.foo>
2025-11-17 11:05:08 -0800

    Comment out seed function body

--- src/app/api/seed/route.ts
+++ src/app/api/seed/route.ts
@@ -3,7 +3,8 @@ import { advocates } from "../../../db/schema";
 import { advocateData } from "../../../db/seed/advocates";
 
 export async function POST() {
-  const records = await db.insert(advocates).values(advocateData).returning();
+  // N.B.: Below fails to typecheck, possibly related to: https://github.com/drizzle-team/drizzle-orm/issues/2940
+  // const records = await db.insert(advocates).values(advocateData).returning();
 
-  return Response.json({ advocates: records });
+  // return Response.json({ advocates: records });
 }
